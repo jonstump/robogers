ebde544965c04a152118b86a2979df8ada6715c0
Jon Stump <jmstump@gmail.com>
2021-01-22 09:03:40 -0800

    add first attempt at function robogers1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  //Business Logic
+  let numbers = []
+
+  function robogersBeep(numArray) {
+    for (let i = 0; i < array.length; i++) {
+      if ((array[i]) === "*1*")
+        numbers.push("Beep!")
+        console.log(numArray);
+        console.log(numbers);
+    }
+  }
+
+  const narray = ["1", "10", "15"]
+  const numArray = narray.split();
+  console.log(numArray);
+
+  //User Logic
+  $('form').submit(function(submit) {
+    submit.preventDefault();
+    const input = $('input#numbers').val();
+    const numArray = input.split(" ");
+
+    function robogers(numArray) {
+      robogersBeep(numArray);
+      robogersBoop(numArray);
+      robogersNeighbor(numArray);
+    }
+
+  })
+
+})
