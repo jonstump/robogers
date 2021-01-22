701d965af5503affa5ee99ab65e70e97e99f77c4
Jon Stump <jmstump@gmail.com>
2021-01-22 15:44:10 -0800

    continue to attempt solutions to get the first function to pass it's test.

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,27 @@ $(document).ready(function() {
   }
 console.log(robogersCountUp(15)) 
 
+
+  const numbersString = numbers.map(function(element) {
+    return element.toString();
+  });
+  numbersString;
+
+
 function robogersBeep(numbers) {
+  let numbersString = numbers.toString();
+  let numbersStringSplit = numbersString.split(",");
+  for (array of numbersStringSplit)
   for (let i = 0; i < array.length; i++) {
-    if ((array[i]) === 1) {
-      outputArray.push("Beep!")
+    if ((array[i][0]) === "*3*") {
+      outputArray.push("Won't you be my neighbor?")
       console.log(outputArray);
     } else {
     (outputArray.push(array))
     } 
   }
 }
-
+I have no idea how I'm supposed to convert this array to a string without an error message.
 
 
 
@@ -50,6 +60,24 @@ console.log(numberString);
   $('form').submit(function(submit) {
     submit.preventDefault();
     const input = $('input#userNumber').val();
+    let numbers = [];
+
+    function robogersCountUp(input) {
+      for (let i = 0; i <= input; i++) {
+        numbers.unshift(i);
+      }
+      return numbers
+    }
+
+console.log(input())    
+    
+    
+    
+    
+    
+    
+    
+    
     const numArray = input.split("");
     const stringArray = numArray.toString();
     console.log(numArray);
