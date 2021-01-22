19aa41e0f5d5536c0c6f22274a4455118a5c832c
Jon Stump <jmstump@gmail.com>
2021-01-22 14:06:17 -0800

    add a commit to show that I'm struggling through changing my new array to a string so this will work.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
   //Business Logic
   let outputArray = [];
   let numbers = [];
-  let numbersString = numbers.map(function(e) {
-    return e.toString()
-  });
 
   function robogersCountUp(input) {
     for (let i = 0; i <= input; i++) {
@@ -16,18 +13,18 @@ $(document).ready(function() {
 console.log(robogersCountUp(15)) 
 
   function robogersNeighbor(numbers) {
-    const numberToString = number.toString();
-    for (const number of numbers){
-        if ((numberToString) === "*3*") {
+    const numberToString = numbers.toString();
+    for (const number of numbersToString){
+        if ((number) === "*3*") {
           outputArray.push("Won't you be my neighbor?")
-        } else if ((numberToString) !== "*3*") {
+        } else if ((numbers) !== "*3*") {
           (outputArray.push(number))
         } else {
           break;
         }
       }  
     }
-console.log(robogersNeighbor(numbersString))
+
 
 
 
